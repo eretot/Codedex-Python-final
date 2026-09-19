a91a92b63ccf33f1c3111ba5b45dce0ccf3144e2
eretot <taaniel.sildre@gmail.com>
2026-09-19 22:05:33 +0300

    Update blog_generator.py
    Refactored blog generator to use environment variables for API key and added user input loop for generating multiple paragraphs.

--- Codeacademy/blog_generator.py
+++ Codeacademy/blog_generator.py
@@ -1,6 +1,12 @@
-import openai
 
-openai.api_key = ''
+
+import openai
+from dotenv import dotenv_values
+
+config = dotenv_values('.env')
+
+openai.api_key = config['API_KEY']
+
 def generate_blog(paragraph_topic):
   response = openai.completions.create(
     model = 'gpt-3.5-turbo-instruct',
@@ -8,9 +14,15 @@ def generate_blog(paragraph_topic):
     max_tokens = 400,
     temperature = 0.3
   )
-
   retrieve_blog = response.choices[0].text
-
   return retrieve_blog
 
-print(generate_blog('Why NYC is better than your city.'))
+keep_writing = True
+
+while keep_writing:
+  answer = input('Write a paragraph? Y for yes, anything else for no. ')
+  if (answer == 'Y'):
+    paragraph_topic = input('What should this paragraph talk about? ')
+    print(generate_blog(paragraph_topic))
+  else:
+    keep_writing = False
